45c5c8f3cc2be45206a5e9e6971148ea3f7a30f3
Robin <randersson@anaconda.com>
2026-08-10 11:01:28 -0400

    Update requirements

--- constructor/conda_interface.py
+++ constructor/conda_interface.py
@@ -102,15 +102,9 @@ if conda_interface_type == "conda":
     distro = None
     if sys.platform.startswith("linux"):
         try:
-            import distro
+            import distro  # noqa: F401  # re-exported for use in preconda.py
         except ImportError:
             pass
-
-        if distro is None:
-            try:
-                from conda._vendor import distro
-            except ImportError:
-                pass
 
     def get_repodata(url):
         if CONDA_MAJOR_MINOR >= (23, 5):
